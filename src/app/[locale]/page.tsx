0686d56a5d9cf61bd024f86c5ba7e4384321b004
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-24 14:32:08 -0500

    Update Homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,52 @@
+import Particles from '@/components/home/particles';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+
 
 export default function Home() {
   const t = useTranslations('Home');
+  const n = useTranslations('Navbar');
+  const navigation = [
+    { name: n('about'), href: "/about" },
+    { name: n('projects'), href: "/projects" },
+    { name: n('projects'), href: "/contact" },
+  ];
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <h1>{t('title')}</h1>;
-    </main>
+		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+			<nav className="my-16 animate-fade-in">
+				<ul className="flex items-center justify-center gap-4">
+					{navigation.map((item) => (
+						<Link
+							key={item.href}
+							href={item.href}
+							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+						>
+							{item.name}
+						</Link>
+					))}
+				</ul>
+			</nav>
+      <div className="my-16 text-center animate-fade-in">
+				<h2 className="text-sm text-zinc-500 ">
+					{t('title')}
+				</h2>
+			</div>
+
+			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+			<Particles
+				className="absolute inset-0 -z-10 animate-fade-in"
+				quantity={100}
+			/>
+			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+				lordksix
+			</h1>
+
+			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+			<div className="my-16 text-center animate-fade-in">
+				<h2 className="text-sm text-zinc-500 ">
+					{t('descrip1')}
+				</h2>
+			</div>
+		</div>
   )
 }
