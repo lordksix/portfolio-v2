87ae620f02bc9cdf1716c423e413688d39bea723
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-26 05:26:28 -0500

    Update page.tsx

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -74,19 +74,19 @@ const AboutPage = () => {
     },
   ];
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+    <main className="flex flex-col items-center justify-center w-screen h-screen pt-8 overflow-hidden">
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
         <h1 className="z-10 text-3xl text-transparent duration-1000 cursor-default bg-sky-700 dark:bg-white text-edge-outline animate-title font-display sm:text-4xl md:text-5xl whitespace-nowrap bg-clip-text ">
           {t('title')}
         </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-8 text-center animate-fade-in">
+      <div className="my-6 text-center animate-fade-in">
         <h2>
           <p className="text-lg font-extrabold sm:text-xl md:text-2xl">Wladimir P.</p>
           <p className="sm:text-lg">{t('subtitle')}</p>
         </h2>
       </div>
-      <div className="flex flex-col w-8/12 mx-auto my-8 animate-fade-in sm:grid sm:grid-cols-2 sm:gap-x-10">
+      <div className="flex flex-col w-8/12 mx-auto my-8 animate-fade-in sm:grid sm:grid-cols-2 sm:gap-x-14">
         <div className="flex flex-col justify-center">
           <p>{t('descrip1')}</p>
           <br />
