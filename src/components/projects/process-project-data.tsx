c56215a6a7c2cc165906b5bcb8469ff8940e6d95
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-31 20:37:56 -0500

    Update process-project-data.tsx

--- src/components/projects/process-project-data.tsx
+++ src/components/projects/process-project-data.tsx
@@ -1,55 +1,82 @@
+import Link from 'next/link';
 import React, { ReactNode } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { SiGithub } from 'react-icons/si';
+import { VscLiveShare } from 'react-icons/vsc';
 
 type processedData = {
   name: ReactNode,
   description: ReactNode,
-  url: string,
-  repo: string,
+  url: ReactNode,
   snapshot: string,
   techtag: ReactNode,
 }
 
-const processTechtag = (techtag: string[]) => {
+const processBullet = (element: string[]) => {
   const techNode: ReactNode = (
     <>
-      {techtag.map((tag) => (
+      {element.map((elem) => (
         <span
           key={uuidv4()}
           className="px-2 py-1 text-white rounded bg-sky-900 hover:bg-sky-700 dark:bg-white dark:text-black dark:hover:bg-gray-300"
         >
-          {tag}
+          {elem}
         </span>
       ))}
     </>
   );
   return techNode;
-}
-const processData = (data: projectData[], name: string, description: string, techtag: string) => {
+};
+
+const processLinks = (element: string[], elementTag: string[]) => {
+  const urlNode: ReactNode = (
+    <>
+      {element.map((elem, i) => (
+        <Link
+          key={uuidv4()}
+          rel="noreferrer"
+          target="_blank"
+          className="flex items-center justify-between gap-2 px-2 py-2 text-white rounded bg-sky-900 hover:bg-sky-700 dark:bg-white dark:text-black dark:hover:bg-gray-300"
+          href={elem}
+        >
+          {i === 0 ? <VscLiveShare className="w-8 h-8"/> : <SiGithub className="w-8 h-8"/>}
+          {elementTag[i]}
+        </Link>
+      ))}
+    </>
+  );
+  return urlNode;
+};
+
+const processData = (data: projectData[], name: string, description: string, techtag: string, urlName: string[]) => {
   const processedData: processedData[] = [];
    data.forEach((dataElement, i) => {
     const elementName: ReactNode = (
-      <h4 className="text-4xl font-semibold tracking-wide flex flex-col cursor-default text-[#F7AB0A]/70">
+      <h4 className="text-3xl font-semibold tracking-wide flex flex-col cursor-default text-[#F7AB0A]/70">
         {dataElement[name as keyof projectData]}
       </h4>
     );
     const elementDescrip: ReactNode = (
-      <p className="text-lg text-justify cursor-default">
+      <p className="w-8/12 mx-auto text-justify cursor-default sm:w-11/12">
         {dataElement[description as keyof projectData]}
       </p>
     );
     const elementTechTag: ReactNode = (
-      <div className="text-lg text-justify cursor-default">
-        {processTechtag(dataElement[techtag as keyof projectData] as string[])}
+      <div className="grid w-10/12 grid-cols-2 mx-auto text-sm text-justify cursor-default sm:grid-cols-3 md:grid-cols-4 gap-x-4 gap-y-2">
+        {processBullet(dataElement[techtag as keyof projectData] as string[])}
+      </div>
+    );
+    const elementURL: ReactNode = (
+      <div className="flex flex-col w-full gap-2 text-sm cursor-default sm:flex-row sm:justify-around">
+        {processLinks(dataElement.url, urlName)}
       </div>
     );
     processedData[i] = {
       name: elementName,
       description: elementDescrip,
       techtag: elementTechTag,
-      url: dataElement.url,
+      url: elementURL,
       snapshot: dataElement.snapshot,
-      repo: dataElement.repo,
     };
   });
   return processedData;
