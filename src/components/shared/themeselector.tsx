bac8d8e0acbe134db4abd6d32a7c0df4a501d417
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-18 15:52:28 -0500

    Change color in themeSlector y lanaguage Dropdown

--- src/components/shared/themeselector.tsx
+++ src/components/shared/themeselector.tsx
@@ -17,7 +17,7 @@ const DarkModeButton = () => {
   }
 
   return (
-    <button onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')} className="flex items-center justify-start w-6 h-6 bg-transparent rounded active:scale-90 hover:bg-sky-900 hover:text-white dark:hover:text-black dark:hover:bg-white">
+    <button onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')} className="flex items-center justify-start w-6 h-6 bg-transparent rounded active:scale-90 text-[#414ec7] hover:bg-[#414ec7] hover:text-white dark:text-[#408f44] dark:hover:bg-white">
         {theme === 'dark' ? <FaMoon className="w-4 h-4 mx-auto"/> : <FaSun className="w-4 h-4 mx-auto"/>}
     </button>
   );
