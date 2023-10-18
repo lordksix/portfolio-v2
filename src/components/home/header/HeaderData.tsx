326f9652b05e63ce13da6bdface664f6403b1088
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-18 11:31:19 -0500

    Restructure Header

--- src/components/home/header/HeaderData.tsx
+++ src/components/home/header/HeaderData.tsx
@@ -11,20 +11,23 @@ import LoadingData from '../../shared/loadingData';
 import { useRouter } from 'next/navigation';
 import { GoHomeFill } from 'react-icons/go';
 import DarkModeButton from '@/components/shared/themeselector';
-
+import { useTranslations } from 'next-intl';
 
 type Props = {
-  show: boolean,
-  links: {
-    path: string;
-    text: string;
-  }[],
+  show: boolean
 }
 
-const HeaderData = ({ show = false, links }: Props) => {
+const HeaderData = ({ show = false }: Props) => {
   const scrolled = useScroll(50);
   const router = useRouter();
   const { MenuModal, setShowMenuModal } = useMenuModal();
+
+  const n = useTranslations('Navbar');
+  const links = [
+    { text: n('about'), path: "/about" },
+    { text: n('projects'), path: "/projects" },
+    { text: n('contact'), path: "/contact" },
+  ];
 
   const returnBtn = (
     <button type="button" className="flex items-center justify-center hover:text-blue-500 active:scale-90" onClick={() => router.back()}  title="goback">
@@ -33,7 +36,7 @@ const HeaderData = ({ show = false, links }: Props) => {
   );
   const logo = (
     <div className="flex items-center gap-1">
-      <Link href="/" className="flex flex-wrap items-center active:scale-90 justify-center gap-1 px-2 py-1 rounded-md hover:text-white/90 hover:bg-sky-900 dark:hover:text-black dark:hover:bg-white/90">
+      <Link href="/" className="flex flex-wrap items-center justify-center gap-1 px-2 py-1 rounded-md active:scale-90 hover:text-white/90 hover:bg-sky-900 dark:hover:text-black dark:hover:bg-white/90">
         <GoHomeFill />
         <Image
           src="/lordksix-logos_transparent.png"
