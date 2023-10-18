e54a4632edcca5ffb9b1f4809b717fad0298bd74
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-10-18 15:53:51 -0500

    Change color in footer, projects and about and contact page

--- src/components/home/footer/footer.tsx
+++ src/components/home/footer/footer.tsx
@@ -8,25 +8,25 @@ const Footer = () => {
   const t = useTranslations('Home');
   const c = useTranslations('Common');
   return (
-    <footer className="w-10/12 animate-fade-in md:w-8/12">
+    <footer className="w-10/12 py-4 animate-fade-in md:w-8/12">
       <div className="flex justify-between">
         <div>
-          <p className="text-sm sm:text-base">{t('connect')}</p>
-          <div className="flex justify-start gap-2">
+          <p className="text-sm sm:text-base font-bold text-[#414ec7] dark:text-[#408f44]">{t('connect')}</p>
+          <div className="flex justify-start gap-2 text-[#414ec7] dark:text-[#408f44]">
             <Link href={'https://twitter.com/wapasquel'} rel="noreferrer" target="_blank">
-              <FaSquareTwitter className="w-8 h-8 hover:bg-sky-900 hover:text-white active:scale-90 dark:hover:text-black dark:hover:bg-white"/>
+              <FaSquareTwitter className="w-8 h-8 hover:bg-sky-900 hover:text-white dark:bg-white active:scale-90 dark:hover:text-white dark:hover:bg-[#408f44]"/>
             </Link>
             <Link href={'https://www.linkedin.com/in/wladpasquel/'} rel="noreferrer" target="_blank">
-              <FaLinkedin className="w-8 h-8 hover:bg-sky-900 hover:text-white active:scale-90 dark:hover:text-black dark:hover:bg-white"/>
+              <FaLinkedin className="w-8 h-8 hover:bg-sky-900 hover:text-white dark:bg-white active:scale-90 dark:hover:text-white dark:hover:bg-[#408f44]"/>
             </Link>
             <Link href={'https://medium.com/@wladimir.pasquel/'} rel="noreferrer" target="_blank">
-              <FaMedium className="w-8 h-8 hover:bg-sky-900 hover:text-white active:scale-90 dark:hover:text-black dark:hover:bg-white"/>
+              <FaMedium className="w-8 h-8 hover:bg-sky-900 hover:text-white dark:bg-white active:scale-90 dark:hover:text-white dark:hover:bg-[#408f44]"/>
             </Link>
             <Link href={'https://github.com/lordksix/'} rel="noreferrer" target="_blank">
-              <FaGithub className="w-8 h-8 hover:bg-sky-900 hover:text-white active:scale-90 dark:hover:text-black dark:hover:bg-white"/>
+              <FaGithub className="w-8 h-8 hover:bg-sky-900 hover:text-white dark:bg-white active:scale-90 dark:hover:text-white dark:hover:bg-[#408f44]"/>
             </Link>
             <Link href={'https://wellfound.com/u/wpasquel/'} rel="noreferrer" target="_blank">
-              <FaAngellist className="w-8 h-8 hover:bg-sky-900 hover:text-white active:scale-90 dark:hover:text-black dark:hover:bg-white"/>
+              <FaAngellist className="w-8 h-8 hover:bg-sky-900 hover:text-white dark:bg-white active:scale-90 dark:hover:text-white dark:hover:bg-[#408f44]"/>
             </Link>
           </div>
         </div>
